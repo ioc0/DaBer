b0849c1787f54353f6d314efaf2f6eb96ed08fbb
Alexander Kubkin <morttraine@outlook.com>
2017-03-20 14:55:31 +0300

    Первый тест

---
+++ DayutBeri/DayutBeri/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+
+namespace DayutBeri
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public int tick = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void textInputed(object sender, TextCompositionEventArgs e)
+        {
+            if (e.Text == "q")
+            {
+                this.Close();
+            }
+            if (e.Text == "a")
+            {
+                rectangle.Fill = new SolidColorBrush(Colors.Green);
+                tick++;
+                ticketCount.Content = "Количество электронных билетов: "+ tick.ToString();
+            }
+            if (e.Text == "b")
+            {
+                rectangle.Fill = new SolidColorBrush(Colors.Red);
+
+            }
+        }
+    }
+}
